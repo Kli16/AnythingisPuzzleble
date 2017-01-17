e6c7dee0998aafee2257d5a652e1da74e351a810
eric.zhang <ezhang2@stuy.edu>
2017-01-17 08:02:04 -0500

    Started Art Puzzle

---
+++ ArtPuzzle.java
@@ -0,0 +1,20 @@
+public class ArtPuzzle {
+
+    String[][] Grid = new String[3][3];
+    Grid = [["01","02","03"],
+	    ["04","05","06"],
+	    ["07","08","  "]];
+
+    public static void moveHorizontal(String direction) {
+	int rowNumber;
+	for (int row = 0; row < Grid.length; row++) {
+	    if (Grid[row].toString().indexOf("  ") > -1) {
+		rowNumber = row;
+		break;
+	    }
+	}
+	if 
+	
+
+    public static void main(String[] args) {
+	

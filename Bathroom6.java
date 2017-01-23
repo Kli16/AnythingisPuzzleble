8dea43da568561eb70e739a43679995734c3055f
dimahvirstuy <dhvirtsman@stuy.edu>
2017-01-22 22:31:15 -0500

    Created Bathroom 6 and 7

--- Bathroom6.java
+++ Bathroom6.java
@@ -1,5 +1,31 @@
 public class Bathroom6 extends Activity{
     public void Play(Player p){
-	
+	boolean win = Game();
+	if(win) {
+	    if (p.GPA == 1) {
+		System.out.println("You find a key on the ground and remember teh message that was left in the 7th floor bathroom ~ '5th floor bathroom closed until key is found, if found please go to 206, DO NOT OPEN THE DOOR ON YOUR OWN'");
+	    }
+	    else {
+		System.out.println("You find a key on the ground, unsure what you might need it for you keep it on you for safekeeping");
+	    }
+	    p.GPA += 1;
+	}
+	else {
+	    System.out.println("You leave the 6th floor bathroom empty-handed");
+	}
+	p.energy += 5;
+    }
+    public boolean Game() {
+	System.out.println("You find yourself in a pristine bathroom, not a scratch on it, Would you like to look around?(Yes or No)");
+	String look = Keyboard.ReadString();
+	if (look.equals("Yes")) {
+		return true;
+	    }
+	    else {
+		System.out.println("Well there seems to be nothing left to do");
+		return false;
+	    }
     }
 }
+
+//CHECK IF GPA IS 1, IF SO REMEMBERS THAT THIS IS THE KEY TO BATHROOM5
--- Bathroom7.java
+++ Bathroom7.java
@@ -1,5 +1,34 @@
+import cs1.Keyboard;
+
 public class Bathroom7 extends Activity{
     public void Play(Player p){
-	
+	boolean win = Game();
+	if(win) {
+	    System.out.println("You find a strange note that seems to have been posted on the front of the door, it reads '5th floor bathroom closed until key is found, if found please go to 206, DO NOT OPEN THE DOOR ON YOUR OWN'.");
+	    p.GPA += 1;
+	    p.energy += 5;
+	}
+	else {
+	    System.out.println("You leave the bathroom empty handed, maybe for the best");
+	    p.energy += 5;
+	}
     }
+    public boolean Game() {
+	System.out.println("You walk into the 7th floor bathroom and you find it completely ransacked, look around?(y or n)");
+	String look = Keyboard.readString();
+	if (look.equals("y")) {
+	    System.out.println("You look around and see the mirror smashed to bits, the trash can toppled down, and a piece of paper on the ground, what would you like to do?");
+	    System.out.println("what do you want to observe?");
+	    String thing = Keyboard.readString();
+	    for(int i = 0; i < thing.length() - 4; i++) {
+		if (thing.substring(i, i + 5).equals("paper")) {
+		    return true;
+		}
+	    }
+	    System.out.println("You find nothing of particular importance");
+	    return false;
+	}
+	return false;
+    }
+    
 }
